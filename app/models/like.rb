3ad84146854d2484d01a9a3ac8dd25422fd37b46
chandevcode <baguschandradp@gmail.com>
2023-03-02 11:07:54 +0800

    add model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,16 @@ class Like < ApplicationRecord
 
   belongs_to :user
   belongs_to :post
+
+  after_create_commit do
+    broadcast_update_to 'posts_list', target: "likes-post-count-#{post_id}",
+                                      partial: 'posts/likes_count',
+                                      locals: { count: post.likes.count }
+  end
+
+  after_destroy_commit do
+    broadcast_update_to 'posts_list', target: "likes-post-count-#{post_id}",
+                                      partial: 'posts/likes_count',
+                                      locals: { count: post.likes.count }
+  end
 end
